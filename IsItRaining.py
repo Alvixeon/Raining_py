11f10cdc3c3e6f669355e6f5c9425686a5773ee1
Alvixeon <32423825+Alvixeon@users.noreply.github.com>
2018-09-18 12:14:19 -0500

    Update IsItRaining.py

--- IsItRaining.py
+++ IsItRaining.py
@@ -6,7 +6,7 @@ class Resources():
     locationXpath = '//*[@id="page"]/tbody/tr[3]/td/table/tbody/tr/td[1]/table[7]/tbody/tr[5]/td[2]'
     locationUrl = "https://www.infobyip.com/"
     rainingXpath = '/html/body/div/div/div[1]/h1//text()'
-    rainingUrl = "https://isitraining.in/Eau-Claire"
+    rainingUrl = "https://isitraining.in/"
     
 #Get the users GeoLocation so that we can check their specific area for rain
 def GetLocation():
@@ -18,10 +18,13 @@ def GetLocation():
 #Check if is raining wherever the user is located
 def IsItRaining(location):
     location = location.replace(" ", "-")
-    pageContent=requests.get(Resources.rainingUrl, headers = {'User-agent': 'Location'})
+    url = Resources.rainingUrl + location
+    pageContent=requests.get(url, headers = {'User-agent': 'Location'})
     tree = html.fromstring(pageContent.content)
     caption = tree.xpath(Resources.rainingXpath)
     if "No" in caption:
         print ("It's not currently raining in %s" % location)
+    else:
+        print ("It's currently raining in %s" % location)
 
 GetLocation()
